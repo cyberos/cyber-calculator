c6fffc069fa60cb9aa05d3dca4c26948ae889fd7
Weblate <noreply@weblate.org>
2021-02-20 21:28:16 +0100

    Added translation using Weblate (Sinhala)

---
+++ translations/si_LK.ts
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="si" sourcelanguage="de_DE">
+<context>
+    <name>CTextField</name>
+    <message>
+        <location filename="../qml/CTextField.qml" line="40"/>
+        <source>Cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/CTextField.qml" line="46"/>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/CTextField.qml" line="52"/>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="13"/>
+        <source>Calculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
