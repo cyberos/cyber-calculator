ca9c82ea8b8790c018c36812e0d5df8b4dbc2f94
Tom Longhorn <tom_longhorn@protonmail.com>
2021-02-20 16:51:22 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CyberOS/Calculator
    Translate-URL: https://hosted.weblate.org/projects/cyberos/calculator/cs/

--- translations/cs_CZ.ts
+++ translations/cs_CZ.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../qml/CTextField.qml" line="40"/>
         <source>Cut</source>
-        <translation type="unfinished"></translation>
+        <translation>Vyjmout</translation>
     </message>
     <message>
         <location filename="../qml/CTextField.qml" line="46"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopírovat</translation>
     </message>
     <message>
         <location filename="../qml/CTextField.qml" line="52"/>
         <source>Paste</source>
-        <translation type="unfinished"></translation>
+        <translation>Vložit</translation>
     </message>
 </context>
 <context>
@@ -24,7 +24,7 @@
     <message>
         <location filename="../qml/main.qml" line="13"/>
         <source>Calculator</source>
-        <translation type="unfinished"></translation>
+        <translation>Kalkulačka</translation>
     </message>
 </context>
 </TS>
