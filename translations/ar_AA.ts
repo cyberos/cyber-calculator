be20cd13e7dfcecc1bf5a578f6aee0b49273f15f
Tameem Alshebel <tamoomy2004@gmail.com>
2021-05-05 13:21:44 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: CyberOS/Calculator
    Translate-URL: https://hosted.weblate.org/projects/cyberos/calculator/ar/

--- translations/ar_AA.ts
+++ translations/ar_AA.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../qml/CTextField.qml" line="40"/>
         <source>Cut</source>
-        <translation type="unfinished">يقطع</translation>
+        <translation>قطع</translation>
     </message>
     <message>
         <location filename="../qml/CTextField.qml" line="46"/>
         <source>Copy</source>
-        <translation type="unfinished">Copy</translation>
+        <translation>نسخ</translation>
     </message>
     <message>
         <location filename="../qml/CTextField.qml" line="52"/>
         <source>Paste</source>
-        <translation type="unfinished">معجون</translation>
+        <translation>لصق</translation>
     </message>
 </context>
 <context>
@@ -24,7 +24,7 @@
     <message>
         <location filename="../calcengine.cpp" line="38"/>
         <source>Error: %1</source>
-        <translation type="unfinished">خطأ:٪ 1</translation>
+        <translation>خطأ:٪ 1</translation>
     </message>
     <message>
         <location filename="../engine/evaluator.cpp" line="1041"/>
@@ -40,32 +40,32 @@
         <location filename="../engine/evaluator.cpp" line="1267"/>
         <location filename="../engine/evaluator.cpp" line="1292"/>
         <source>Invalid expression.</source>
-        <translation type="unfinished">تعبير غير صالح.</translation>
+        <translation>تعبير غير صالح.</translation>
     </message>
     <message>
         <location filename="../engine/evaluator.cpp" line="1062"/>
         <location filename="../engine/evaluator.cpp" line="1068"/>
         <location filename="../engine/evaluator.cpp" line="1074"/>
         <source>Variable cannot be overwritten.</source>
-        <translation type="unfinished">لا يمكن الكتابة فوق المتغير.</translation>
+        <translation>لا يمكن الكتابة فوق المتغير.</translation>
     </message>
     <message>
         <location filename="../engine/evaluator.cpp" line="1081"/>
         <source>Identifier matches an existing function name.</source>
-        <translation type="unfinished">المعرف يطابق اسم وظيفة موجود.</translation>
+        <translation>المعرف يطابق اسم وظيفة موجود.</translation>
     </message>
     <message>
         <location filename="../engine/evaluator.cpp" line="1165"/>
         <location filename="../engine/evaluator.cpp" line="1205"/>
         <location filename="../engine/evaluator.cpp" line="1222"/>
         <source>Division by zero.</source>
-        <translation type="unfinished">القسمة على صفر.</translation>
+        <translation>القسمة على صفر.</translation>
     </message>
     <message>
         <location filename="../engine/evaluator.cpp" line="1245"/>
         <location filename="../engine/evaluator.cpp" line="1261"/>
         <source>Unknown function or variable.</source>
-        <translation type="unfinished">وظيفة أو متغير غير معروف.</translation>
+        <translation>وظيفة أو متغير غير معروف.</translation>
     </message>
 </context>
 <context>
@@ -73,7 +73,7 @@
     <message>
         <location filename="../qml/main.qml" line="13"/>
         <source>Calculator</source>
-        <translation type="unfinished">آلة حاسبة</translation>
+        <translation>آلة حاسبة</translation>
     </message>
 </context>
 </TS>
