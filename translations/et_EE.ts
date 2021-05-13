7716ac52d460af017329e8a87c392fae79a21f60
Anonymous <noreply@weblate.org>
2021-05-13 08:48:09 +0000

    Translated using Weblate (Estonian)
    Currently translated at 50.0% (5 of 10 strings)
    
    Translation: CyberOS/Calculator
    Translate-URL: https://hosted.weblate.org/projects/cyberos/calculator/et/

--- translations/et_EE.ts
+++ translations/et_EE.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../qml/CTextField.qml" line="46"/>
         <source>Copy</source>
-        <translation>Kopeeri</translation>
+        <translation type="unfinished">Kopeeri</translation>
     </message>
     <message>
         <location filename="../qml/CTextField.qml" line="52"/>
